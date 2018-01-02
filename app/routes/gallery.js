a401ef4a009fce6d6dc7cd5b3727281af660f568
Gareth Dsouza <gdsouza@usc.edu>
2018-01-01 20:00:59 -0800

    added gallery id to routes

--- app/routes/gallery.js
+++ app/routes/gallery.js
@@ -2,17 +2,30 @@ var express = require('express');
 var router = express.Router();
 var image = require('../models/images');
 
-/* GET users listing. */
+/* GET images listing. */
 router.get('/', function(req, res, next) {
+    const imageID = req.query.id || null;
+    let openImage = {};
     image.getImages((err, results) => {
             if(err){
                 console.log(err);
                 res.render('gallery', { message: 'Error in getting data', error: err });
                 return;
             }
-            res.render('gallery', { title: 'Gallery', imagesData: results });
-    });
 
+            if(imageID){
+                console.log("THIS IS CALLED");
+                openImage = results.filter((obj) => {
+                    return obj.ID == imageID
+                })[0];
+
+            }
+
+            res.render('gallery', { title: 'Gallery', imagesData: results, openImage });
+
+
+    });
 });
+
 
 module.exports = router;
